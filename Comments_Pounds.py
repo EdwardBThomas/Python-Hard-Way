a820bcfe63dee70363557dbe15daf11da9a7d860
Edward Thomas <ebthomas@live.com>
2024-01-23 14:46:45 -0800

    Chapter 2 of Learning Pythin the Hard way.

---
+++ Comments_Pounds.py
@@ -0,0 +1,6 @@
+#A comment, so you can read your program later.
+#Anything after the # sign on a line is ignored in Python. And most object-oriented languages, really.
+print("I could have code like this.")# and the comment afterwards is ignored.
+#You can also use a comment to disable or comment out code:
+#print("This will not run.")
+print("This will run.")
