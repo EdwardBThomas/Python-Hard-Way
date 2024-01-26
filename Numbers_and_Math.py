a3cc878dcbad651214e9411add5671447a94c05e
Edward Thomas <ebthomas@live.com>
2024-01-26 11:41:06 -0800

    Chapter 3

---
+++ Numbers_and_Math.py
@@ -0,0 +1,25 @@
+#Prints to the shell.
+print("Time to count the chickens:")
+#Adds then divides.
+print("Hens", 25 + 30 / 6)
+#Subtracts, multiplies, divides.
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now to count the eggs:")
+#Performs the four major operators.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+#A binary true/false statement.
+print(3 + 2 < 5 - 7)
+#Prints text output, then performs a calculation/an operation.
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
