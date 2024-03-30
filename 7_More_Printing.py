959c112999bc3b9d36e174b8940f4e89e2a7efdb
Edward Thomas <ebthomas@live.com>
2024-03-29 19:29:40 -0700

    29March2024_1800

---
+++ 7_More_Printing.py
@@ -0,0 +1,22 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere which Mary went.")
+print("." * 10) #what did that do?
+
+var1 = "C"
+var2 = "h"
+var3 = "e"
+var4 = "e"
+var5 = "s"
+var6 = "e"
+var7 = "B"
+var8 = "u"
+var9 = "r"
+var10 = "g"
+var11 = "e"
+var12 = "r"
+
+# watch var = ' ' at the var; try removing it to see what happens
+print(var1 + var2 + var3 + var4 + var5 + var6)
+#print(var1 + var2 + var3 + var4 + var5 + var6, var=' ')
+print (var7 + var8 + var9 + var10 + var11 + var12)
