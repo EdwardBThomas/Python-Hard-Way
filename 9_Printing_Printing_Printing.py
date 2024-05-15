64268e476fc6838a8a963df5594b734dccbc4cc8
Edward Thomas <ebthomas@live.com>
2024-05-14 22:18:36 -0700

    14May2024@22:18

---
+++ 9_Printing_Printing_Printing.py
@@ -0,0 +1,14 @@
+#some strange, new stuff--remember to type it exactamundo.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+      There's something going on here.
+      With three double-quotes.
+      We'll be able to type as much as we like.
+      Even four lines if we want, or five, or six.
+      """)
